e590c2a516b44fad7d261b6b401b15ee451a88e1
NandaNxD <nanven9@gmail.com>
2021-12-15 12:14:57 +0530

    FIREBASE AUTHENTICATION ADDED

--- HomeDog.py
+++ HomeDog.py
@@ -1,4 +1,17 @@
 from firebase_admin import firestore
 import firebase_admin
+from firebase_admin import credentials
+
+#GLOBAL VARIABLES
+PATH_TO_SERVICEACCOUNTKEY="watchdog-4475a-firebase-adminsdk-rue2z-bd330db374.json"
+PIR_PIN=4
 
 #firestore.firestore.SERVER_TIMESTAMP
+
+def firebase_authentication(PATH_TO_SERVICEACCOUNTKEY):
+    cred = credentials.Certificate(PATH_TO_SERVICEACCOUNTKEY)
+    firebase_admin.initialize_app(cred)
+
+if __name__=="__main__":
+    firebase_authentication(PATH_TO_SERVICEACCOUNTKEY)
+    
