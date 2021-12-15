eb6fbca3ad2a38011905b4ab1c5fdaad3a058ea3
NandaNxD <nanven9@gmail.com>
2021-12-15 12:36:04 +0530

    Firebase connected with homedog

--- HomeDog.py
+++ HomeDog.py
@@ -1,17 +1,43 @@
 from firebase_admin import firestore
 import firebase_admin
+import time
 from firebase_admin import credentials
+#from gpiozero import MotionSensor
+#from picamera import PiCamera
 
 #GLOBAL VARIABLES
-PATH_TO_SERVICEACCOUNTKEY="watchdog-4475a-firebase-adminsdk-rue2z-bd330db374.json"
-PIR_PIN=4
+PATH_TO_SERVICEACCOUNTKEY="WatchDog\watchdog-4475a-firebase-adminsdk-rue2z-bd330db374.json"
+PIRSENSOR_PIN=4
 
-#firestore.firestore.SERVER_TIMESTAMP
 
-def firebase_authentication(PATH_TO_SERVICEACCOUNTKEY):
+# def detectMotion():
+#     pir = MotionSensor(PIRSENSOR_PIN)
+#     camera = PiCamera()
+#     try:
+#         while True:
+#             pir.wait_for_motion()
+#             camera.capture()
+#             print("Motion detected!")
+#             time.sleep(10)
+            
+#     except Exception:
+#         camera.close()
+
+
+
+def firebaseAuthentication():
     cred = credentials.Certificate(PATH_TO_SERVICEACCOUNTKEY)
     firebase_admin.initialize_app(cred)
 
+def sendNotification():
+    db=firestore.client()
+    home=db.collection('home').document()
+    home.set({
+        'time':firestore.firestore.SERVER_TIMESTAMP
+    })
+
+    
 if __name__=="__main__":
-    firebase_authentication(PATH_TO_SERVICEACCOUNTKEY)
+    firebaseAuthentication()
+    sendNotification()
     
